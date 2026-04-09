21645bb13b63bbc5882aa63f58697a03da634d3a
root <root@PC-20250807UZGW.localdomain>
2026-04-09 17:18:58 +0800

    exp: FINAL_LR_FRAC 0.08

--- train.py
+++ train.py
@@ -463,7 +463,7 @@ WEIGHT_DECAY = 0.2      # cautious weight decay for Muon
 ADAM_BETAS = (0.8, 0.95) # Adam beta1, beta2
 WARMUP_RATIO = 0.0      # fraction of time budget for LR warmup
 WARMDOWN_RATIO = 0.45   # fraction of time budget for LR warmdown (longer mid-plateau)
-FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
+FINAL_LR_FRAC = 0.08    # final LR as fraction of initial (nonzero tail)
 
 # Model size
 DEPTH = 8               # number of transformer layers
