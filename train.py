a52b3caf808e10b346cdbd185be0da7f44c89edd
root <root@PC-20250807UZGW.localdomain>
2026-04-10 01:57:35 +0800

    rope_theta 502k- (nudge from best 9ec849f line)

--- train.py
+++ train.py
@@ -501,7 +501,7 @@ def build_model_config(depth):
         sequence_len=MAX_SEQ_LEN, vocab_size=vocab_size,
         n_layer=depth, n_head=num_heads, n_kv_head=n_kv_head, n_embd=model_dim,
         window_pattern=WINDOW_PATTERN,
-        rope_theta=502_000.0,  # 490k worse than 500k default; tiny bump above 500k
+        rope_theta=503_000.0,  # 502k beat 500k; +1k nudge from best line
     )
 
 config = build_model_config(DEPTH)
