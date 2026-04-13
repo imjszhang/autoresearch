c4520fee3397284fd142510c03a9fb2ae9095b8a
root <root@PC-20250807UZGW.localdomain>
2026-04-13 15:23:53 +0800

    exp: enable torch.compile for higher throughput

--- train.py
+++ train.py
@@ -523,7 +523,7 @@ optimizer = model.setup_optimizer(
     weight_decay=WEIGHT_DECAY,
 )
 
-# model = torch.compile(model, dynamic=False)  # disabled for initial validation
+model = torch.compile(model, dynamic=False)  # more steps in fixed wall-clock budget
 
 train_loader = make_dataloader(tokenizer, DEVICE_BATCH_SIZE, MAX_SEQ_LEN, "train")
 x, y, epoch = next(train_loader)  # prefetch first batch
